645949bdc8974790c73d70516b7ef95b468ed160
Matias <niz.matias@gmail.com>
2020-11-28 15:49:55 -0300

    css props number format

--- src/numberFormat.js
+++ src/numberFormat.js
@@ -12,9 +12,11 @@ const CommonNumberFormat = (props) => {
   const { format } = props
   const otherProps = { ...props }
   delete otherProps.format
+  console.log({ otherProps })
   const NumberFormatCustom = ({ inputRef, onChange, ...props }) => (
     <NumberFormat
       {...format}
+      {...props}
       getInputRef={inputRef}
       onValueChange={({ value }) => {
         onChange({
